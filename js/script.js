b70d5041184c4a0c100fae9e73409c785bb99ccc
FreekAlberti <federicoalberi96@gmail.com>
2020-08-28 17:10:35 +0200

    funzione casuale e check

--- js/script.js
+++ js/script.js
@@ -12,3 +12,62 @@ con difficoltà 0 => tra 1 e 100
 con difficoltà 1 =>  tra 1 e 80
 con difficoltà 2 => tra 1 e 50
 */
+
+var bombe = 16;
+//var tentativi = 100;
+​
+//funzione che genera un numero casuale tra 1 e 100
+
+function numeriCasuali() {
+  return Math.floor(Math.random() * (100 - 1) + 1);
+}
+​
+//funzione che dato un array e un numero mi controlla che il numero non sia già nell'array
+
+function checkNum(){
+  for (var i = 0; i < listaNumBomba.length; i++) {
+    var check = true;
+    if (numero == listaNumBomba[i]) {
+      check = false;
+    } 
+  }
+  return check;
+}
+​
+//creo variabile array che contiene i numeri bomba
+
+var listaNumBomba = [];
+​
+//ciclo che chiama la mia funzione di generazione dei numeri 16 volte  while(i<bombe)
+//check che il numero non sia duplicato
+//se non è duplicato, lo salvo nell'array bombe
+
+var i = 0;
+while (i < bombe) {
+  var numero = numeriCasuali();
+  var verifica = checkNum();
+  if (verifica == true) {
+    listaNumBomba.push(numero);
+  }
+  i++;
+}
+
+​
+//creo variabile array che contiene i numeri utente
+​
+//variabili booleana haPerso = false
+​
+//ciclo di 100-16 volte  //for(i=0; i<tentativi-bombe; i++)
+  //prompt in cui chiedo all'utente un numero
+  //check che il numero sia compreso tra 1 e 100
+  //check che il numero non sia duplicato nell'array utente
+  //se non è duplicato, salvo il numero nell'array utente
+  //check che il numero non sia nell'array bomba
+  //se il numero è nell'array bomba -> alert hai perso
+      //alert con array utente .length -> numero di volte che l'utente ha inserito un numero corretto
+      //haPerso = true;
+      //esco dal ciclo
+​
+//controllo se haPerso è false
+  //alert hai vinto
+  //alert con array utente .length -> numero di volte che l'utente ha inserito un numero
